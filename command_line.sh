e9d6537806786bef872083953ea9c493d9d6c054
LucaFabri99 <113453613+LucaFabri99@users.noreply.github.com>
2022-11-15 15:54:34 +0100

    final command line (hopefully)

--- command_line.sh
+++ command_line.sh
@@ -9,21 +9,21 @@ do
   # We can get the data of the attributes that we need (numPeopleVisited, numPeopleWant, placeAddress) using the function cut.
   # With the command grep we get the data only for the country that we are interested in.
   echo Number of places that can be found in ${country} :
-  nb_places=$(cut -f 3,4,8 locations_data.tsv | grep "${country}" | wc -l)
+  nb_places=$(cut -f 3,4,8 final_dataset.csv | grep "${country}" | wc -l)
   echo $nb_places
 
   # col 1 = numPeopleVisited
   i=0
-  for numPeopleVisited in $(cut -f 3,4,8 locations_data.tsv | grep "${country}" | cut -f 1); do
+  for numPeopleVisited in $(cut -f 3,4,8 final_dataset.csv | grep "${country}" | cut -f 1); do
     i=$(( $i+$numPeopleVisited ))
   done
   echo Average visit of the places of ${country} :
   i=$(( $i/$nb_places ))
   echo $i
   
-  # col 1 = numPeopleWant
+  # col 2 = numPeopleWant
   k=0
-  for numPeopleWant in $(cut -f 3,4,8 locations_data.tsv | grep "${country}" | cut -f 2); do
+  for numPeopleWant in $(cut -f 3,4,8 final_dataset.csv | grep "${country}" | cut -f 2); do
     k=$(( $k+$numPeopleWant ))
   done
   echo People that want to visit the places of ${country} :
