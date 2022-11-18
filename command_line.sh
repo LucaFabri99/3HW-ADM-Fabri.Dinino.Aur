4a5d5a99813d5dd6867fa87b64e8765c395f0718
LucaFabri99 <luca1000fabri@gmail.com>
2022-11-18 18:58:01 +0100

    final commit command line

--- command_line.sh
+++ command_line.sh
@@ -3,31 +3,25 @@
 countries=("Italy" "Spain" "France" "England" "United_States")
 for country in ${countries[@]};
 do
-  country=${country/_/ } # in order to obtain 'United States' instead of 'United_States'
+  country=${country/_/ } # in order to get 'United States' instead of 'United_States'
   echo Country : ${country}
-  
+
   # We can get the data of the attributes that we need (numPeopleVisited, numPeopleWant, placeAddress) using the function cut.
   # With the command grep we get the data only for the country that we are interested in.
-  echo Number of places that can be found in ${country} :
-  nb_places=$(cut -f 3,4,8 final_dataset.csv | grep "${country}" | wc -l)
-  echo $nb_places
+  nb_places=$(cut -f 3,4,8 final_dataset.tsv | grep "${country}" | wc -l)
+  echo Number of places that can be found in ${country} : $nb_places
 
-  # col 1 = numPeopleVisited
+
+  #We use a counter because we need the average visit of the places, the second cut gets the first column of the columns we already get, which means the 3 column of our dataset
   i=0
-  for numPeopleVisited in $(cut -f 3,4,8 final_dataset.csv | grep "${country}" | cut -f 1); do
+  for numPeopleVisited in $(cut -f 3,4,8 final_dataset.tsv | grep "${country}" | cut -f 1); do
     i=$(( $i+$numPeopleVisited ))
   done
-  echo Average visit of the places of ${country} :
-  i=$(( $i/$nb_places ))
-  echo $i
-  
-  # col 2 = numPeopleWant
-  k=0
-  for numPeopleWant in $(cut -f 3,4,8 final_dataset.csv | grep "${country}" | cut -f 2); do
-    k=$(( $k+$numPeopleWant ))
-  done
-  echo People that want to visit the places of ${country} :
-  echo $k
+  echo Average visit of the places of ${country} : $(( $i/$nb_places ))
 
+
+   #Here we use a new command awk because we only need the sum of the people that want to visit
+    echo People that want to visit the places of ${country} : $(cut -f 3,4,8 final_dataset.tsv | grep "${country}" | awk '{total = total + $2}END{print total}')
+  
   echo
 done
